f8e57fcc221abe1d199a721743036536a4897998
jurgens101 <jurgensftw@gmail.com>
2024-08-16 00:28:31 +0200

    small fix on project bankerRoulette

--- day4/bankerRoulette.py
+++ day4/bankerRoulette.py
@@ -1,7 +1,7 @@
 import random
 #ask the user to enter 5 names
 names = str(input("Enter 5 names then press enter to see who will pay!\n"))
-names = names.split()
+names = names.split(", ")
 
 #randomizing a "winner" to pay the bill
 randomizedName = random.randint(0,4)
