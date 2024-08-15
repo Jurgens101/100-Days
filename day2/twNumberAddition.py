8a219f3b21b6c32946b3c5256a31849ee93827cf
jurgens101 <jurgensftw@gmail.com>
2024-08-15 16:20:12 +0200

    remove duplicate

--- day2/twNumberAddition.py
+++
@@ -1,7 +0,0 @@
-#ask user for a 2 digit number
-userNumber = int(input("Enter a two digit number: "))
-userNumber = str(userNumber)
-numOne = userNumber[0]
-numTwo = userNumber[1]
-
-print (numOne,numTwo)
