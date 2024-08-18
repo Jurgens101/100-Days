2e20e166f0fdcf4299fbb0e377fa051006ff9536
jurgens101 <jurgensftw@gmail.com>
2024-08-18 23:14:12 +0200

    fix hangMan project

--- day7/hangManPart1.py
+++ day7/hangManPart1.py
@@ -8,31 +8,96 @@ triesCount = 0
 magicWord = random.choice(wordList)
 wordLength = len(magicWord)
 status = False
+mistakes = []
 playerLives = 5
-
-print(magicWord)
-print(len(magicWord))
+hangManArt = [
+'''
+ +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''',
+'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''',
+'''
+  +---+
+  |   |
+  O   |
+ / \  |
+      |
+      |
+=========
+''',
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''',
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''',
+'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========='''
+]
+finalArt = hangManArt[::-1]
 
 for i in range (wordLength):
      finalChoice += "_"
-print(finalChoice)
 
+print(finalArt[5])
 
 while not status and playerLives > 0:
     
-    userChoice = input("Enter a letter: \n")
+    userChoice = input("Enter a character: \n")
     for i  in range(wordLength):
         letter = magicWord[i] 
         if letter == userChoice:
-            finalChoice[i] = letter
+                if userChoice in finalChoice:
+                    print(f"You have already entered {userChoice}")
+                else:
+                    finalChoice[i] = letter
+                    print(f"You have entered: {finalChoice}")
+    if userChoice not in magicWord:
+        if userChoice in mistakes:
+                    print(f"You have already entered {userChoice}")
         else:
-            playerLives -=1   
+            playerLives -= 1
+            mistakes.append(userChoice)
+            print(f"{userChoice} is not in the word")
+            print(f"You have entered these wrong words: {mistakes}")
+            print(f"You have {playerLives} Lives left!")
+            print(finalArt[playerLives])
     if "_" not in finalChoice:
-        status = True
-    print(finalChoice)
-    print(f"You have {playerLives} Lives left!")
-
+        print(f"You found the word! It was {magicWord}")
+        status = True  
     if playerLives == 0:
         status = True
         print("Oh no you are out of lives!")
+        print(f"The word was: {magicWord}")
+
 
