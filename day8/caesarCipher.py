ccc161b74430ec6776b7355748d04115b5e3c8fb
jurgens101 <jurgensftw@gmail.com>
2024-08-19 18:28:22 +0200

    update caesarCipher project

--- day8/caesarCipher.py
+++ day8/caesarCipher.py
@@ -1,43 +1,37 @@
 #using caesar cipher to encode and decode a sentence
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-userChoice = input("Do you want to encode or decode?:\n").lower()
-userSentence = input("Enter a message:\n").lower()
-shift = int(input("Enter the shift number:\n"))
+keepGoing = True
 
-def encrypt(sentence, shiftNumber):
-    tempSentence = ""
-    for letter in sentence:
-        if letter in alphabet:
-            x = alphabet.index(letter)
-            newPosition = x + shiftNumber
-            # Handle wrap-around
-            if newPosition >= 26:
-                newPosition = newPosition - 26
-            tempSentence += alphabet[newPosition]
-        else:
-            tempSentence += letter
-    print(f"The encrypted message is: {tempSentence}")
-    return tempSentence
+while keepGoing:
+    userChoice = input("Do you want to encode or decode?:\n").lower()
+    userSentence = input("Enter a message:\n").lower()
+    shift = int(input("Enter the shift number:\n"))
 
-def decrypt(sentence, shiftNumber):
-    tempSentence = ""
-    for letter in sentence:
-        if letter in alphabet:
-            x = alphabet.index(letter)
-            newPosition = x - shiftNumber
-            # Handle wrap-around
-            if newPosition < 0:
-                newPosition = newPosition + 26
-            tempSentence += alphabet[newPosition]
-        else:
-            tempSentence += letter
-    print(f"The decrypted message is: {tempSentence}")
-    return tempSentence
 
-if userChoice == "encode":
-    encrypt(userSentence, shift)
-elif userChoice == "decode":
-    decrypt(userSentence, shift)
-else:
-    print("Please enter a choice between 'encrypt' or 'decrypt'")
+    def caesar(sentence, shiftNumber, direction):
+        tempSentence = ""
+        for letter in sentence:
+            if letter in alphabet:
+                x = alphabet.index(letter)
+                if direction == "encode":
+                    newPosition = x + shiftNumber
+                else:
+                    newPosition = x - shiftNumber
+                # Handle wrap-around
+                if newPosition >= 26:
+                    newPosition = newPosition - 26
+                tempSentence += alphabet[newPosition]
+            else:
+                tempSentence += letter
+        print(f"The encrypted message is: {tempSentence}")
+        return tempSentence
+
+    caesar(userSentence, shift, userChoice)
+    tryAgain = input("Do you want to try again? Yes or No:\n")
+    if tryAgain == "yes":
+            keepGoing = True
+    else:
+            print("Goodbye!")
+            keepGoing = False
+
